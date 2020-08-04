c002a1756c4fb616055881c9078467a8b96a3684
Miacik <cacmiac@gmail.com>
2020-08-04 13:35:42 +0200

    gamefunc done 🎆🎈

---
+++ client/app/game.py
@@ -0,0 +1,6 @@
+import math
+from python import gamefunc 
+
+board = {'board' : [0*i for i in range(100)]}
+
+print(gamefunc.createBoard(board))    
---
+++ client/python/gamefunc.py
@@ -0,0 +1,30 @@
+import random
+
+def createBoard(board:dict):
+    """
+    A method used to create a board with snakes and ladders
+    """
+    arrOfSnakes = []
+    for j in range(2):
+        for i in range(0,6):
+            x = random.randint(0,100)
+            y = random.randint(0,100 - x)
+            while y == x: 
+                y = random.randint(0,100)
+            arrOfSnakes.append((x,y))
+
+        if j == 1:
+            board.update({'snakes':arrOfSnakes})
+            x = 0
+            arrOfSnakes = []
+            y = []
+        else:
+            board.update({'ladders':arrOfSnakes})
+            x = 0
+            arrOfSnakes = []
+            y = []
+            
+       
+
+    return board
+
